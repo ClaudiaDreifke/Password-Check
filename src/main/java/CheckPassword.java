76e3a8576292bf9fa12b4c285b0d3e8b5ad8a2a0
claudia <claudia.dreifke@gmail.com>
2022-07-02 17:07:30 +0200

    add more checks

--- src/main/java/CheckPassword.java
+++ src/main/java/CheckPassword.java
@@ -1,6 +1,65 @@
-public class CheckPassword {
+import java.util.Scanner;
 
-    public static boolean checkPasswordLength(String Password){
-        return Password.length()>7;
+public class CheckPassword {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie ein Passwort ein:");
+        String Password = sc.nextLine();
+
+        if (checkLength(Password) == false) {
+            System.out.println(" Bitte nutzen Sie mindestens 8 Zeichen für Ihr Passwort!");
+        } else if (checkDigits(Password) == false) {
+            System.out.println(" Bitte nutzen Sie Ziffern für Ihr Passwort!");
+        } else if (checkUpperLowerCase(Password) == false) {
+            System.out.println(" Bitte nutzen Sie Groß- und Kleinschreibung für Ihr Passwort!");
+        } else if (checkNoUnsecureWords(Password) == false) {
+            System.out.println(" Bitte nutzen Sie keine unsicheren Wörter für Ihr Passwort!");
+        } else
+            System.out.println("Ihr Passwort ist super!");
+
+    }
+
+    public static boolean checkLength(String Password) {
+        int minimumLength = 8;
+        return Password.length() >= minimumLength;
+    }
+
+    public static boolean checkDigits(String Password) {
+        boolean check = false;
+        int stop = (Password.length() - 1);
+        for (int i = 0; i < stop; i++) {
+            char[] PasswordArray = Password.toCharArray();
+            if (Character.isDigit(PasswordArray[i])) {
+                check = true;
+                break;
+            } else check = false;
+        }
+        return check;
+    }
+
+    public static boolean checkUpperLowerCase(String Password) {
+        boolean checkUpperCase = false;
+        boolean checkLowerCase = false;
+        int stop = (Password.length());
+        for (int i = 0; i < stop; i++) {
+            char[] PasswordArray = Password.toCharArray();
+            if (Character.isUpperCase(PasswordArray[i])) {
+                checkUpperCase = true;
+            } else checkLowerCase = true;
+        }
+        return checkUpperCase && checkLowerCase;
+    }
+
+    public static boolean checkNoUnsecureWords(String Password) {
+        boolean check = true;
+        String[] UnsecureWords = {"Hallo", "Passwort", "Password", "123", "abc", "qwertz", "qwerty"};
+        int stop = (UnsecureWords.length);
+        for (int i = 0; i < stop; i++) {
+            if (Password.equalsIgnoreCase(UnsecureWords[i])) {
+                check = false;
+            }
+        }
+        return check;
     }
 }
--- src/test/java/TestCheckPassword.java
+++ src/test/java/TestCheckPassword.java
@@ -4,28 +4,90 @@ import org.junit.jupiter.api.Test;
 public class TestCheckPassword {
 
     @Test
-    void testCheckPasswordLength_7(){
-
+    void testCheckLength_Fail(){
         // given
         String Password = "1234567";
-
         // when
-        boolean actual = CheckPassword.checkPasswordLength(Password);
-
+        boolean actual = CheckPassword.checkLength(Password);
         // then
         Assertions.assertEquals(false, actual);
     }
 
     @Test
-    void testCheckPasswordLength_8(){
-
+    void testCheckLength_Pass(){
         // given
         String Password = "12345678";
-
         // when
-        boolean actual = CheckPassword.checkPasswordLength(Password);
+        boolean actual = CheckPassword.checkLength(Password);
+        // then
+        Assertions.assertEquals(true, actual);
+    }
+    @Test
+    void testCheckDigits_Fail(){
+        // given
+        String Password = "jklasjd";
+        // when
+        boolean actual = CheckPassword.checkDigits(Password);
+        // then
+        Assertions.assertEquals(false, actual);
+    }
 
+    @Test
+    void testCheckDigits_Pass(){
+        // given
+        String Password = "1233423";
+        // when
+        boolean actual = CheckPassword.checkDigits(Password);
+        // then
+        Assertions.assertEquals(true, actual);
+    }
+
+    @Test
+    void testCheckUpperLowerCase_FailLower(){
+        // given
+        String Password = "kkkkkkk";
+        // when
+        boolean actual = CheckPassword.checkUpperLowerCase(Password);
+        // then
+        Assertions.assertEquals(false, actual);
+    }
+
+    @Test
+    void testCheckUpperLowerCase_FailUpper(){
+        // given
+        String Password = "HSHSJSJS";
+        // when
+        boolean actual = CheckPassword.checkUpperLowerCase(Password);
+        // then
+        Assertions.assertEquals(false, actual);
+    }
+
+    @Test
+    void testCheckUpperLowerCase_Pass(){
+        // given
+        String Password = "HSHSjkjJS";
+        // when
+        boolean actual = CheckPassword.checkUpperLowerCase(Password);
+        // then
+        Assertions.assertEquals(true, actual);
+    }
+    @Test
+    void testCheckNoUnsecureWords_Fail(){
+        // given
+        String Password = "hallo";
+        // when
+        boolean actual = CheckPassword.checkNoUnsecureWords(Password);
+        // then
+        Assertions.assertEquals(false, actual);
+    }
+    @Test
+    void testCheckNoUnsecureWords_Pass(){
+        // given
+        String Password = "hjsakdh";
+        // when
+        boolean actual = CheckPassword.checkNoUnsecureWords(Password);
         // then
         Assertions.assertEquals(true, actual);
     }
 }
+
